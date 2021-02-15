37d2d8f5bb6e2e663e6aee0108db9dfb8e87a747
Chris Rebbelin <s0548921@htw-berlin.de>
2021-02-15 13:36:32 +0100

    Remove typo from user agent string

--- ored-tg-bot/scraper.py
+++ ored-tg-bot/scraper.py
@@ -17,7 +17,7 @@ class OredScraper:
         self.__running = False
         self.__sess = requests.Session()
         self.__sess.headers.update({
-            'User-Agent': 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
         })
         self.__delay = delay
         self.__hds = {
