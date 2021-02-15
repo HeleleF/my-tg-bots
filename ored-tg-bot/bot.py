d38991e31efbf636ae68c725744e104c83c2db92
Chris Rebbelin <s0548921@htw-berlin.de>
2021-02-15 14:43:34 +0100

    Send messages directly from scraper

--- ored-tg-bot/bot.py
+++ ored-tg-bot/bot.py
@@ -27,7 +27,6 @@ log.addHandler(ch)
 # Create the Updater and pass it your bot's token.
 updater = Updater(BOT_AUTH_TOKEN)
 
-pokes_db = dict()
 poke_filter = None
 iv_filter = None
 
@@ -56,6 +55,11 @@ def stop(update: Update, context: CallbackContext) -> None:
 def ping(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('pong')
 
+def db_size(update: Update, context: CallbackContext) -> None:
+
+    size = scraper.get_pokes_db_size()
+    update.message.reply_text(f'Pokes in db: {size}')
+
 def set_filter(update: Update, context: CallbackContext) -> None:
 
     new_filters = 'bub'
@@ -73,64 +77,8 @@ def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
     update.message.reply_text(f'I dont know: "{update.message.text}", check /help')
 
-def create_html_message(e, now_time):
-
-    # format IVs as hex string if they exist
-    if e['individual_attack'] is not None:
-        ivs = f"{e['individual_attack']:X}{e['individual_defense']:X}{e['individual_stamina']:X}"
-    else:
-        ivs = '???'
-
-    if e['level']:
-        lvlcp = f"Lvl {e['level']} - <b>{e['cp']} CP</b>"
-    else:
-        lvlcp = "Lvl ??? - CP ???"
-
-    mytxt = f"{e['pokemon_name']} {lvlcp} ({ivs})\r\nVerified: {'✅' if e['is_verified_despawn'] else '❌'}\r\n"
-
-    despawn_time = time.localtime(e['disappear_time'] / 1000)
-    seconds_left = time.mktime(despawn_time) - now_time
-
-    mins, secs = divmod(seconds_left, 60)
-
-    result = f"{mytxt}Bis: <b>{time.strftime('%H:%M:%S', despawn_time)}</b> (Noch <b>{int(mins)} Min {int(secs)}</b>)"
-
-    return result
-
-def send_encounter(poke, nt):
-
-    html_msg = create_html_message(poke, nt)
-
-    try:
-        updater.bot.send_message(chat_id=BOT_MYSELF_CHAT_ID, text=html_msg, parse_mode=ParseMode.HTML)
-        updater.bot.send_location(chat_id=BOT_MYSELF_CHAT_ID, latitude=poke['latitude'], longitude=poke['longitude'])
-        return True
-    except NetworkError:
-        log.debug('Sending failed')
-        return False
-    
-def on_data_recieved(poke_list):
-
-    now_time = time.mktime(time.localtime())
-
-    for poke in poke_list:
-
-        enc_id = poke.get('encounter_id', '')
-
-        if pokes_db.get(enc_id, False):
-            # log.debug('Already known')
-            continue
-
-        log.debug(f'New encounter with id {enc_id} added')
-        pokes_db[enc_id] = send_encounter(poke, now_time)
-
-
-def on_error(err):
-
-    updater.bot.send_message(chat_id=BOT_MYSELF_CHAT_ID, text=f'Scraper failed with {err}', parse_mode=ParseMode.HTML)
-
 log.debug('Loading scraper')
-scraper = OredScraper(on_data=on_data_recieved, on_error=on_error)
+scraper = OredScraper(tg_bot=updater.bot, chat_id=BOT_MYSELF_CHAT_ID)
 
 def main():
     """Start the bot."""
@@ -141,6 +89,7 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("stop", stop))
+    dispatcher.add_handler(CommandHandler("size", db_size))
     dispatcher.add_handler(CommandHandler("ping", ping))
     dispatcher.add_handler(CommandHandler("set", set_filter))
     dispatcher.add_handler(CommandHandler("help", help_command))
--- ored-tg-bot/scraper.py
+++ ored-tg-bot/scraper.py
@@ -3,9 +3,14 @@
 
 import requests
 import logging
-from threading import Thread
+from threading import Event, Thread
 import time
 import re
+from datetime import datetime
+from pytz import timezone
+
+from telegram.error import NetworkError
+from telegram import ParseMode, Bot
 
 from secrets import DOMAIN, API_ENDPOINT
 
@@ -13,7 +18,7 @@ log = logging.getLogger('ored-tg')
 
 class OredScraper:
 
-    def __init__(self, on_data=None, on_error=None, delay: int=5):
+    def __init__(self, tg_bot: Bot, chat_id: str, delay: int=5):
         self.__running = False
         self.__sess = requests.Session()
         self.__sess.headers.update({
@@ -73,16 +78,23 @@ class OredScraper:
                 "oNeLng": "13.261485099792482",
                 "reids": "",
                 "eids": "0",
-		"exMinIV": "113,149"
+		        "exMinIV": "113,149"
         }
+
+        self.__pokes_db = dict()
         
         self.__scraper_thread = None
         self.__remover_thread = None
+        self.__stopper = Event()
 
-        self.__action_cb = on_data
-        self.__error_cb = on_error
+        self.__tg_bot = tg_bot
+        self.__CHAT_ID = chat_id
+
+        # check for expired encounters every this many seconds
+        self.CLEANUP_EXPIRED_INTERVAL = 600
 
         self.__filters = None
+        self.__tz = timezone('Europe/Berlin')
 
         self.__setup()
 
@@ -95,12 +107,10 @@ class OredScraper:
         if m:
             log.debug(f'Token set to {m[1]}')
             self.__payload['token'] = m[1]
+            self.__tg_bot.send_message(chat_id=self.__CHAT_ID, text='💪🏻 Scraper loaded 💪🏻', parse_mode=ParseMode.HTML)
         else:
-
             log.error(f'No token found!')
-
-            if self.__error_cb:
-                self.__error_cb('No token found!')
+            self.__tg_bot.send_message(chat_id=self.__CHAT_ID, text='❌ No token found! ❌', parse_mode=ParseMode.HTML)
 
     def __apply_filter(self, data):
 
@@ -114,32 +124,22 @@ class OredScraper:
             response.raise_for_status()
 
         except requests.HTTPError as httpe:
-            log.error(f'Get seasons failed with http error: {httpe}')
-            if self.__error_cb:
-                self.__error_cb(httpe)
+            self.__on_error(f'POST failed with http error: {httpe}')
             return []
         except requests.exceptions.ConnectionError as cerr:
-            log.error(f'Get seasons failed with network problems: {cerr}')
-            if self.__error_cb:
-                self.__error_cb(cerr)
+            self.__on_error(f'POST failed with network error: {cerr}')
             return []
         except requests.exceptions.Timeout:
-            log.error('Get seasons timed out!')
-            if self.__error_cb:
-                self.__error_cb('timeout')
+            self.__on_error(f'POST timed out')
             return []
         except requests.exceptions.RequestException as err:
-            log.error(f'Get seasons failed with request error: {err}')
-            if self.__error_cb:
-                self.__error_cb(err)
+            self.__on_error(f'POST failed with request error: {err}')
             return []
 
         try:
             data = response.json()
         except ValueError:
-            log.error(f'Recieved non-json response: {response.text}')
-            if self.__error_cb:
-                self.__error_cb(f'Recieved non-json response: {response.text}')
+            self.__on_error(f'Recieved non-json response: {response.text}')
             return []
 
         try:
@@ -151,6 +151,42 @@ class OredScraper:
 
         return self.__apply_filter(pokes)
 
+    def __send_encounter(self, poke, now: int):
+
+        # format IVs as hex string if they exist
+        if poke['individual_attack'] is not None:
+            ivs = f"{poke['individual_attack']:X}{poke['individual_defense']:X}{poke['individual_stamina']:X}"
+        else:
+            ivs = '???'
+
+        if poke['level']:
+            lvlcp = f"Lvl {poke['level']} - <b>{poke['cp']} CP</b>"
+        else:
+            lvlcp = "Lvl ??? - CP ???"
+
+        mytxt = f"{poke['pokemon_name']} {lvlcp} ({ivs})\r\nVerified: {'✅' if poke['is_verified_despawn'] else '❌'}\r\n"
+
+        despawn_time = poke['disappear_time'] / 1e3
+
+        mins, secs = divmod(int(despawn_time) - now, 60)
+
+        html_msg = f"{mytxt}Bis: <b>{datetime.fromtimestamp(despawn_time, tz=self.__tz).strftime('%H:%M:%S')}</b> (Noch <b>{int(mins)} Min {int(secs)}</b>)"
+
+        try:
+            self.__tg_bot.send_message(chat_id=self.__CHAT_ID, text=html_msg, parse_mode=ParseMode.HTML)
+            self.__tg_bot.send_location(chat_id=self.__CHAT_ID, latitude=poke['latitude'], longitude=poke['longitude'])
+
+            self.__pokes_db[poke['encounter_id']] = True
+
+        except NetworkError:
+            log.debug('Sending failed')
+            self.__pokes_db[poke['encounter_id']] = False
+
+    def __on_error(self, err):
+
+        log.error(err)
+        self.__tg_bot.send_message(chat_id=self.__CHAT_ID, text=f'Scraper failed with {err}', parse_mode=ParseMode.HTML)
+
     def __scraping_loop(self, filters = None):
         """ Repeatedly gets data and calls the callback with it """
 
@@ -159,12 +195,33 @@ class OredScraper:
 
         while self.__running:
 
-            data = self.__get_data()
+            filtered_pokes = self.__get_data()
+            now_time = int(datetime.now(self.__tz).timestamp())
 
-            if self.__action_cb and len(data):
-                self.__action_cb(data)
+            for poke in filtered_pokes:
+
+                enc_id = poke.get('encounter_id', '')
+
+                # already in db, ignore
+                if self.__pokes_db.get(enc_id, False):
+                    continue
+
+                log.debug(f'New encounter with id {enc_id} added')
+                self.__send_encounter(poke, now_time)
 
             time.sleep(self.__delay)
+
+    def __removing_loop(self):
+
+        # repeat until stop flag is set
+        while not self.__stopper.wait(self.CLEANUP_EXPIRED_INTERVAL):
+            now = int(datetime.now(self.__tz).timestamp())
+            log.debug('Removing...')
+
+            # iterate through database and remove expired encounters
+            for enc_id, p_info in self.__pokes_db.copy().items():
+                if p_info['despawn'] - now < 5:
+                    del self.__pokes_db[enc_id]
 
     def start(self, filters, *args):
         """ Runs the scraper by starting a separate thread that runs the scraper loop """
@@ -173,8 +230,11 @@ class OredScraper:
             log.debug('Already running')
             return
 
-        self.__running = True
+        self.__stopper.clear()
+        self.__remover_thread = Thread(target=self.__removing_loop)
+        self.__remover_thread.start()
 
+        self.__running = True
         self.__scraper_thread = Thread(target=self.__scraping_loop, args=args)
         self.__scraper_thread.start()
         log.debug(f'Started with filters: {filters}')
@@ -193,9 +253,21 @@ class OredScraper:
         log.debug('Stopped scraper thread!')
         self.__scraper_thread = None
 
+        self.__stopper.set()
+
+        log.debug('Stopping cleaner thread...')
+        self.__remover_thread.join()
+        log.debug('Stopped cleaner thread!')
+        self.__remover_thread = None
+
+        self.__pokes_db = dict()
+
     def update_filters(self, filters):
 
         self.__filters = filters
+
+    def get_pokes_db_size(self):
+        return len(self.__pokes_db)
 
     def is_running(self):
         """ Whether the scraper is currently running """
